859d732751b9ae2021e6418c5efd20c87df0ecac
tetsu-26 <heiwa326427@gmial.com>
2019-04-25 00:05:36 +0900

    商品出品

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_060455) do
+ActiveRecord::Schema.define(version: 2019_04_24_013926) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.string "image"
+    t.string "state", null: false
+    t.integer "price", null: false
+    t.string "local"
+    t.bigint "user_id"
+    t.bigint "buyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +40,6 @@ ActiveRecord::Schema.define(version: 2019_04_23_060455) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
+  add_foreign_key "items", "users", column: "buyer_id"
 end
