6ea579a287708c0ea8ec018730b496ae9df12c02
tetsu-26 <heiwa326427@gmial.com>
2019-04-30 20:48:40 +0900

    商品詳細、編集、削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :destroy, :update]
 
   def index
     @items = Item.all
@@ -17,11 +18,35 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    render :new
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   def show
-    
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :user_id, :description, :state, :price, :local)
   end
